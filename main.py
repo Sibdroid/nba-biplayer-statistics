0cc890e766df0f81c54249997d7f5c45c39cf1ea
Sibdroid <77882767+Sibdroid@users.noreply.github.com>
2022-08-31 18:22:20 +0700

    Updated main.py with two more requests

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import visualizing
+import time
 def main() -> None:
+    visualizing.draw_gif((3975, 6475), ("Curry", "Thompson"), "gsw", "2022", 1.2)
+    time.sleep(3)
+    visualizing.draw_gif((614, 110), ("O'Neal", "Bryant"), "lal", "2000-2002", 1.2)
+    time.sleep(3)
     visualizing.draw_gif((1966, 1987), ("James", "Wade"), "mia", "2010-2014", 1.2)
 if __name__ == "__main__":
     main()
